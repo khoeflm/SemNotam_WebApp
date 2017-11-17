cdb3afbf6608ca46b74d5971ebc9cd2b830d3f4b
khoeflm <k.hoeflmeier@gmx.at>
2017-11-17 18:24:36 +0100

    ...

---
+++ src/main/java/at/notamWebapp/util/validator/MyValidationEventHandler.java
@@ -0,0 +1,27 @@
+package at.notamWebapp.util.validator;
+/*
+  SemNOTAM Project (User Interface)
+  Created by khoef on 26.07.2017.
+ */
+
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventHandler;
+
+public class MyValidationEventHandler implements ValidationEventHandler {
+
+    public boolean handleEvent(ValidationEvent event) {
+        System.out.println("\nEVENT");
+        System.out.println("SEVERITY:  " + event.getSeverity());
+        System.out.println("MESSAGE:  " + event.getMessage());
+        System.out.println("LINKED EXCEPTION:  " + event.getLinkedException());
+        System.out.println("LOCATOR");
+        System.out.println("    LINE NUMBER:  " + event.getLocator().getLineNumber());
+        System.out.println("    COLUMN NUMBER:  " + event.getLocator().getColumnNumber());
+        System.out.println("    OFFSET:  " + event.getLocator().getOffset());
+        System.out.println("    OBJECT:  " + event.getLocator().getObject());
+        System.out.println("    NODE:  " + event.getLocator().getNode());
+        System.out.println("    URL:  " + event.getLocator().getURL());
+        return true;
+    }
+
+}
